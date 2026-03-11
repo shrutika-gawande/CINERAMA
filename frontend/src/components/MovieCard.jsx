2701d79b02010bbc8fa7950c25a64c493c250aca
shrutika-gawande <shrutikagawande80@gmail.com>
2026-03-11 16:32:43 +0530

    updated moviecard

--- frontend/src/components/MovieCard.jsx
+++ frontend/src/components/MovieCard.jsx
@@ -1,10 +1,11 @@
-export default function MovieCard() {
+export default function MovieCard({movie}) {
     return (
         <div className="card" style={{ width: "18rem" }}>
-            <img src="..." className="card-img-top" alt="..." />
+            <img src={movie.poster} className="card-img-top" alt="..." />
             <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+                <h5 className="card-title">{movie.title}</h5>
+                <p className="card-text">{movie.year} • {movie.genre}</p>
+                <p>Rating</p>
                 {/* <a href="#" className="btn btn-primary">Go somewhere</a> */}
             </div>
         </div>
