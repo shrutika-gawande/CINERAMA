0a4641d9cae6ae23e7b2b92538163f47081d217a
shrutika-gawande <shrutikagawande80@gmail.com>
2026-03-15 11:51:55 +0530

    footer added

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,10 @@
+import '../styles/footer.css'
+
+export default function Footer() {
+    return (
+        <footer>
+            <div class="footer-logo">CINERAMA</div>
+            <div class="footer-text">© 2025 Cinerama. All rights reserved.</div>
+        </footer>
+    );
+}
