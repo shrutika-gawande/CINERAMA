41dcfbaee1b4acfe120fe05e470b863cc4faa6b7
shrutika-gawande <shrutikagawande80@gmail.com>
2026-03-17 17:39:27 +0530

    added components in app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,17 @@
-import { useState } from 'react'
 import MovieDetails from './pages/MovieDetails'
+import Home from './pages/Home'
+import Search from './pages/Search'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 function App() {
 
   return (
     <>
+      <Navbar/>
+      <Home />
+      <Search />
       <MovieDetails />
+      <Footer/>
     </>
   )
 }
