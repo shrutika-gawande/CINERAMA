587459b919c0eddcdaff040abed7fcb51acaa6f4
shrutika-gawande <shrutikagawande80@gmail.com>
2026-03-19 23:09:38 +0530

    added movie modal

--- frontend/src/pages/MovieDetails.jsx
+++ frontend/src/pages/MovieDetails.jsx
@@ -1,13 +1,32 @@
+import { useState } from "react";
 import MovieCard from "../components/MovieCard";
+import MovieModal from "../components/MovieModal";
 import movies from "../utils/movieData";
-import "../styles/moviePage.css"
+import "../styles/moviePage.css";
 
 export default function MovieDetails() {
+  // null = modal closed  |  movie object = modal open for that movie
+  const [selectedMovie, setSelectedMovie] = useState(null);
+
   return (
-    <div className="moviecards">
-      {movies.map((movie) => (
-        <MovieCard key={movie.id} movie={movie} />
-      ))}
-    </div>
+    <>
+      <div className="moviecards">
+        {movies.map((movie) => (
+          <MovieCard
+            key={movie.id}
+            movie={movie}
+            onOpen={setSelectedMovie}   // pass setter as onOpen prop
+          />
+        ))}
+      </div>
+
+      {/* Only mounts when a movie is selected */}
+      {selectedMovie && (
+        <MovieModal
+          movie={selectedMovie}
+          onClose={() => setSelectedMovie(null)}
+        />
+      )}
+    </>
   );
 }
