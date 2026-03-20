c255f957cfb7e5f42527db877ce1f5789e04feb5
shrutika-gawande <shrutikagawande80@gmail.com>
2026-03-20 23:55:09 +0530

    added home page and its styling

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import '../styles/home.css'
+
+export default function Home() {
+    return (
+        <section className='hero-section'>
+            <div>
+                <h1>Rate & Discover<br/><em>Great Films</em></h1>
+                <p className='hero-sub'>Curated cinema for discerning viewers. Browse, rate, and find your next obsession.</p>
+            </div>
+        </section>
+    );
+}
